4c8f91430c9d3976e43e24940f75e9579afb0760
Julien B <MisterJBro@protonmail.com>
2021-03-04 23:35:51 +0100

    Continued baseline

--- Pyrado/scripts/training/qq-su_ppo_gae.py
+++ Pyrado/scripts/training/qq-su_ppo_gae.py
@@ -1,5 +1,5 @@
 """
-Baseline for policy distillation. Baseline model using domain randomization and ppo.
+Train an agent to solve the Quanser Qube swing-up task using Proximal Policy Optimization with Generalized Advantage Estimation.
 """
 import torch as to
 from multiprocessing import freeze_support
@@ -7,29 +7,17 @@ from multiprocessing import freeze_support
 import pyrado
 from pyrado.algorithms.step_based.ppo_gae import PPOGAE
 from pyrado.environment_wrappers.action_normalization import ActNormWrapper
-from pyrado.domain_randomization.default_randomizers import create_default_randomizer
-from pyrado.environment_wrappers.action_delay import ActDelayWrapper
-from pyrado.environment_wrappers.observation_noise import GaussianObsNoiseWrapper
-from pyrado.environment_wrappers.domain_randomization import DomainRandWrapperLive
-from pyrado.environments.pysim.quanser_cartpole import QCartPoleSwingUpSim
+from pyrado.environment_wrappers.observation_normalization import ObsNormWrapper
 from pyrado.environments.pysim.quanser_qube import QQubeSwingUpSim
+from pyrado.utils.data_types import RenderMode
 from pyrado.policies.feed_forward.fnn import FNNPolicy
 from pyrado.logger.experiment import setup_experiment, save_dicts_to_yaml
-from pyrado.policies.special.environment_specific import QCartPoleSwingUpAndBalanceCtrl
+from pyrado.sampling.rollout import rollout
 from pyrado.utils.argparser import get_argparser
 from pyrado.utils.data_types import EnvSpec
 from pyrado.spaces import ValueFunctionSpace
 import multiprocessing as mp
-import argparse
 
-# Parameters
-parser = argparse.ArgumentParser()
-
-# Environment
-parser.add_argument('--frequency', type=int, default=250)
-parser.add_argument('--env_type', type=str, default='qcp-su')
-parser.add_argument('--max_steps', type=int, default=8_000)
-parser.add_argument('--seed', type=int, default=None)
 
 if __name__ == "__main__":
     # For multiprocessing and float32 support, recommended to include at top of script
@@ -37,28 +25,17 @@ if __name__ == "__main__":
     to.set_default_dtype(to.float32)
 
     # Parse command line arguments
-    args = parser.parse_args()
+    args = get_argparser().parse_args()
 
-    # Environment
-    env_hparams = dict(dt=1 / args.frequency, max_steps=args.max_steps)
-    
     # Experiment (set seed before creating the modules)
-    if args.env_type == 'qcp-su':
-        ex_dir = setup_experiment(QCartPoleSwingUpSim.name, f"{PPOGAE.name}_baseline_{QCartPoleSwingUpAndBalanceCtrl.name}")
-        env = QCartPoleSwingUpSim(**env_hparams)
-    elif args.env_type == 'qq-su':
-        ex_dir = setup_experiment(QQubeSwingUpSim.name, f"{PPOGAE.name}_baseline_{QQubeSwingUpSim.name}")
-        env = QQubeSwingUpSim(**env_hparams)
-    #print(env)
+    ex_dir = setup_experiment(QQubeSwingUpSim.name, f"{PPOGAE.name}_{FNNPolicy.name}", f"250Hz_seed_{args.seed}")
 
     # Set seed if desired
     pyrado.set_seed(args.seed, verbose=True)
 
-    # Domain Randomization
-    randomizer = create_default_randomizer(env)
-    env = DomainRandWrapperLive(env, randomizer)
-    env = ActNormWrapper(env)
-    env = ActDelayWrapper(env, delay=3)
+    # Environment
+    env_hparams = dict(dt=1 / 250.0, max_steps=8_000)
+    env = ActNormWrapper(QQubeSwingUpSim(**env_hparams))
 
     # Policy
     policy_hparam = dict(hidden_sizes=[64, 64], hidden_nonlin=to.relu, output_nonlin=to.tanh)
@@ -73,16 +50,21 @@ if __name__ == "__main__":
     critic_hparam = dict(hidden_sizes=[64, 64], hidden_nonlin=to.relu)
     critic = FNNPolicy(spec=EnvSpec(env.obs_space, ValueFunctionSpace), **critic_hparam)
 
+    # Reduce weights of last layer, recommended by paper
+    #for p in critic.net.output_layer.parameters():
+    #    with to.no_grad():
+    #        p /= 100
+
     # Subroutine
     algo_hparam = dict(
-        max_iter=100,
+        max_iter=50,
         tb_name="ppo",
         traj_len=8_000,
         gamma=0.99,
         lam=0.97,
         env_num=9,
         cpu_num=min(9,mp.cpu_count()-1),
-        epoch_num=40,
+        epoch_num=60,
         device="cpu",
         max_kl=0.05,
         std_init=0.6,
@@ -103,3 +85,12 @@ if __name__ == "__main__":
     # Jeeeha
     algo.train(snapshot_mode="best", seed=args.seed)
     
+    # Test policy
+    while True:
+        input('Press some key to continue:')
+        ro = rollout(
+            env,
+            algo.expl_strat,
+            render_mode=RenderMode(text=True, video=True),
+            eval=True,
+        )
