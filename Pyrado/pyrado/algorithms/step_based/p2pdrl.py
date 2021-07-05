3eacd4824068367cc4c780ecbfdb61dee575a8bc
Julien <MisterJBro@protonmail.com>
2021-07-05 16:59:25 +0200

    Started p2pdrl
    Co-authored-by: bhahner <bhahner@users.noreply.github.com>

---
+++ Pyrado/pyrado/algorithms/step_based/p2pdrl.py
@@ -0,0 +1,334 @@
+# Copyright (c) 2020, Fabio Muratore, Honda Research Institute Europe GmbH, and
+# Technical University of Darmstadt.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of Fabio Muratore, Honda Research Institute Europe GmbH,
+#    or Technical University of Darmstadt, nor the names of its contributors may
+#    be used to endorse or promote products derived from this software without
+#    specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL FABIO MURATORE, HONDA RESEARCH INSTITUTE EUROPE GMBH,
+# OR TECHNICAL UNIVERSITY OF DARMSTADT BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import os
+from copy import deepcopy
+
+import numpy as np
+import torch as to
+
+import pyrado
+from pyrado.algorithms.base import Algorithm, InterruptableAlgorithm
+from pyrado.domain_randomization.default_randomizers import create_default_randomizer
+from pyrado.domain_randomization.domain_randomizer import DomainRandomizer
+from pyrado.environments.base import Env
+from pyrado.exploration.stochastic_action import NormalActNoiseExplStrat
+from pyrado.logger.experiment import Experiment, ask_for_experiment
+from pyrado.logger.step import StepLogger
+from pyrado.policies.base import Policy
+from pyrado.utils.experiments import load_experiment
+from pyrado.sampling.envs import Envs
+from pyrado.algorithms.step_based.ppo_gae import PPOGAE
+
+
+class P2PDRL(PPOGAE):
+    """Online Peer-to-Peer Distillation Reinforcement Learning (P2PDRL). https://arxiv.org/pdf/2012.04839v1.pdf."""
+
+    name: str = "p2pdrl"
+
+    def __init__(
+        self,
+        save_dir: str,
+        env: Env,
+        policy: Policy,
+        critic: Policy,
+        logger: StepLogger = None,
+        device: str = "cpu",
+        lr: float = 5e-4,
+        std_init: float = 0.1,
+        min_steps: int = 4000,
+        num_epochs: int = 10,
+        max_iter: int = 500,
+        num_workers: int = 8,
+        alpha = 0.1,
+        num_cpu: int = 3,
+        worker_algo: callable = None,
+        worker_algo_hparam: dict() = None,
+        randomizer: DomainRandomizer = None,
+    ):
+        """
+        Constructor
+
+        :param save_dir: directory to save the snapshots i.e. the results in
+        :param env: the environment which the policy operates
+        :param policy: policy to be updated
+        :param logger: logger for every step of the algorithm, if `None` the default logger will be created
+        :param device: device to use for updating the policy (cpu or gpu)
+        :param lr: (initial) learning rate for the optimizer which can be by modified by the scheduler.
+                    By default, the learning rate is constant.
+        :param std_init: initial standard deviation on the actions for the exploration noise
+        :param min_steps: minimum number of state transitions sampled per policy update batch
+        :param num_epochs: number of epochs (how often we iterate over the same batch)
+        :param max_iter: number of iterations (policy updates)
+        :param num_teachers: number of teachers that are used for distillation
+        :param num_cpu: number of cpu cores to use
+        :param teacher_extra: extra dict from PDDRTeachers algo. If provided, teachers are loaded from there
+        :param teacher_policy: policy to be updated (is duplicated for each teacher)
+        :param teacher_algo: algorithm class to be used for training the teachers
+        :param teacher_algo_hparam: hyperparams to be used for teacher_algo
+        :param randomizer: randomizer for sampling the teacher domain parameters. If None, the default one for env is used
+        """
+        if not isinstance(env, Env):
+            raise pyrado.TypeErr(given=env, expected_type=Env)
+        if not isinstance(policy, Policy):
+            raise pyrado.TypeErr(given=policy, expected_type=Policy)
+
+        # Call Algorithm's constructor.
+        super().__init__(
+            save_dir=save_dir, max_iter=max_iter, policy=policy, logger=logger, **worker_algo_hparam
+        )
+
+        # Store the inputs
+        self.min_steps = min_steps
+        self.num_epochs = num_epochs
+        self.num_workers = num_workers
+        self.num_cpu = num_cpu
+        self.device = device
+        self.max_iter = max_iter
+        self.alpha = alpha
+
+        self.algo = worker_algo
+        self.algo_hparam = worker_algo_hparam
+        self.workers_policies = []
+        self.worker_envs = []
+        self.worker_expl_strats = []
+        self.worker_critics = []
+        self.worker_ex_dirs = []
+        self.worker_optimizer = []
+
+        # Add Worker policies, critics, optimizer and expl strats
+        for _ in range(self.num_workers):
+            p = deepcopy(policy)
+            expl = NormalActNoiseExplStrat(p, std_init=std_init)
+            c = deepcopy(critic)
+            o = to.optim.Adam(
+            [
+                {"params": p.parameters()},
+                {"params": expl.parameters()},
+                {"params": c.parameters()},
+            ],
+            lr=lr,
+            weight_decay=1e-5,
+            )
+
+            self.worker_policies.append(p)
+            self.worker_expl_strats.append(expl)
+            self.worker_critics.append(c)
+            self.worker_optimizer.append(o)
+
+        # Environments
+        self.envs = None
+
+        # Distillation loss criterion
+        self.criterion = to.nn.KLDivLoss(log_target=True, reduction="batchmean")
+
+    @property
+    def expl_strat(self) -> NormalActNoiseExplStrat:
+        return self.worker_expl_strats[0]
+
+    @property
+    def policy(self) -> Policy:
+        return self.worker_policies[0]
+
+    def step(self, snapshot_mode: str, meta_info: dict = None):
+        """
+        Performs a single iteration of the algorithm. This includes collecting the data, updating the parameters, and
+        adding the metrics of interest to the logger. Does not update the `curr_iter` attribute.
+
+        :param snapshot_mode: determines when the snapshots are stored (e.g. on every iteration or on new highscore)
+        :param meta_info: is not `None` if this algorithm is run as a subroutine of a meta-algorithm,
+                          contains a dict of information about the current iteration of the meta-algorithm
+        """
+        # Save snapshot to save the correct iteration count
+        self.save_snapshot()
+
+        # Set envs
+        self.set_envs()
+
+        # Sample batch
+        rets = self.sample_batch()
+
+        # Log current progress
+        self.logger.add_value("max return", np.max(rets), 4)
+        self.logger.add_value("median return", np.median(rets), 4)
+        self.logger.add_value("avg return", np.mean(rets), 4)
+        self.logger.add_value("min return", np.min(rets), 4)
+        self.logger.add_value("std return", np.std(rets), 4)
+        self.logger.add_value("std var", self.expl_strat.std.item(), 4)
+
+        # Save snapshot data
+        self.make_snapshot(snapshot_mode, np.mean(rets), meta_info)
+
+        # Update policy and value function
+        for i in range(self.num_workers):
+            self.update(i)
+
+    def update(self, idx):
+        """Update one policy using PPO."""
+        obs, act, rew, ret, adv, dones = self.envs.get_data(self.device)
+        
+        print(obs.shape, act.shape, rew.shape, ret.shape)
+        # Use only the respective observations 
+        obs = obs.reshape(self.num_teachers, self.min_steps, -1)
+        obs = obs[idx]
+
+        # For recurrent pack observations
+        if self.workers_policies[idx].is_recurrent:
+            obs = obs.reshape(-1, self.traj_len, obs.shape[-1])
+            obs_list = []
+            lengths = []
+            for idx, section in enumerate(dones):
+                start = 0
+                for end in section[1:]:
+                    obs_list.append(obs[idx, start:end])
+                    lengths.append(end - start)
+                    start = end
+                if start != self.traj_len:
+                    obs_list.append(obs[idx, start:])
+                    lengths.append(self.traj_len - start)
+            obs = to.nn.utils.rnn.pad_sequence(obs_list)
+            obs = to.nn.utils.rnn.pack_padded_sequence(obs, lengths=lengths, enforce_sorted=False)
+
+        with to.no_grad():
+            if self.workers_policies[idx].is_recurrent:
+                mean, _ = self.workers_policies[idx].rnn_layers(obs)
+                mean, lens = to.nn.utils.rnn.pad_packed_sequence(mean)
+                mean = to.cat([mean[:l, i] for i, l in enumerate(lens)], 0)
+
+                mean = self.workers_policies[idx].output_layer(mean)
+                if self.workers_policies[idx].output_nonlin is not None:
+                    mean = self.workers_policies[idx].output_nonlin(mean)
+            else:
+                mean = self.workers_policies[idx](obs)
+            old_logp = self.worker_expl_strats[idx].action_dist_at(mean).log_prob(act).sum(-1)
+
+        for i in range(self.epoch_num):
+            self.worker_optimizer[idx].zero_grad()
+
+            # Policy
+            if self.workers_policies[idx].is_recurrent:
+                mean, _ = self.workers_policies[idx].rnn_layers(obs)
+                mean, lens = to.nn.utils.rnn.pad_packed_sequence(mean)
+                mean = to.cat([mean[:l, i] for i, l in enumerate(lens)], 0)
+
+                mean = self.workers_policies[idx].output_layer(mean)
+                if self.workers_policies[idx].output_nonlin is not None:
+                    mean = self.workers_policies[idx].output_nonlin(mean)
+            else:
+                mean = self.workers_policies[idx](obs)
+            dist = self.worker_expl_strats[idx].action_dist_at(mean)
+
+            # Critic
+            if self.workers_critics[idx].is_recurrent:
+                val, _ = self.workers_critics[idx].rnn_layers(obs)
+                val, lens = to.nn.utils.rnn.pad_packed_sequence(val)
+                val = to.cat([val[:l, i] for i, l in enumerate(lens)], 0)
+
+                val = self.workers_critics[idx].output_layer(val)
+                if self.workers_critics[idx].output_nonlin is not None:
+                    val = self.workers_critics[idx].output_nonlin(val)
+            else:
+                val = self.workers_critics[idx](obs)
+            val = val.reshape(-1)
+
+            logp = dist.log_prob(act).sum(-1)
+            loss_policy, kl = self.loss_fcn(logp, old_logp, adv)
+
+            # Early stopping if kl divergence too high
+            if kl > self.max_kl:
+                return
+            loss_value = self.criterion(val, ret)
+
+            # Distillation loss
+            loss_dis = 0
+            for i in range(self.num_workers):
+                if i is not idx:
+                    loss_dis += self.criterion(self.workers_policies[i], self.workers_policies[idx])
+            loss_dis /= (self.num_workers-1)
+
+            loss = loss_policy + loss_value + self.alpha * loss_dis
+            loss.backward()
+
+            self.optimizer.step()
+
+    def save_snapshot(self, meta_info: dict = None):
+        super().save_snapshot(meta_info)
+
+        pyrado.save(self.policy, "policy.pt", self.save_dir)
+
+        if meta_info is None:
+            # This algorithm instance is not a subroutine of another algorithm
+            pyrado.save(self.env_real, "env.pkl", self.save_dir)
+
+    def __getstate__(self):
+        # Remove the unpickleable elements from this algorithm instance
+        tmp_teacher_policies = self.__dict__.pop("teacher_policies")
+        tmp_teacher_algo = self.__dict__.pop("teacher_algo")
+        tmp_envs = self.__dict__.pop("envs")
+
+        # Call Algorithm's __getstate__() without the unpickleable elements
+        state_dict = super(P2PDRL, self).__getstate__()
+
+        # Make a deep copy of the state dict such that we can return the pickleable version
+        state_dict_copy = deepcopy(state_dict)
+
+        # Insert them back
+        self.__dict__["teacher_policies"] = tmp_teacher_policies
+        self.__dict__["teacher_algo"] = tmp_teacher_algo
+        self.__dict__["envs"] = tmp_envs
+
+        return state_dict_copy
+
+    def __setstate__(self, state):
+        # Call Algorithm's __setstate__()
+        super().__setstate__(state)
+
+        # Recover settings of environment
+        self.envs = Envs(
+            state["cpu_num"], state["env_num"], state["env"], state["traj_len"], state["gamma"], state["lam"], state["worker_envs"]
+        )
+
+    def set_envs(self):
+        """Creates random environments of the given type."""
+        self.randomizer.randomize(num_samples=self.num_workers)
+        params = self.randomizer.get_params(fmt="dict", dtype="numpy")
+        self.worker_envs = []
+        for e in range(self.num_teachers):
+            self.worker_envs.append(deepcopy(self.env))
+            print({key: value[e] for key, value in params.items()})
+            self.worker_envs[e].domain_param = {key: value[e] for key, value in params.items()}
+
+        self.envs = Envs(
+            min(self.num_cpu, self.num_workers),
+            self.num_teachers,
+            self.env,
+            self.min_steps,
+            self.algo.gamma,
+            self.algo.lam,
+            env_list=self.worker_envs,
+        )
---
+++ Pyrado/scripts/training/qq-su_p2pdrl.py
@@ -0,0 +1,131 @@
+# Copyright (c) 2020, Fabio Muratore, Honda Research Institute Europe GmbH, and
+# Technical University of Darmstadt.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of Fabio Muratore, Honda Research Institute Europe GmbH,
+#    or Technical University of Darmstadt, nor the names of its contributors may
+#    be used to endorse or promote products derived from this software without
+#    specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL FABIO MURATORE, HONDA RESEARCH INSTITUTE EUROPE GMBH,
+# OR TECHNICAL UNIVERSITY OF DARMSTADT BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+"""
+Train an agent to solve the Quanser Qube swing-up task using Online Peer-to-Peer Distillation Reinforcement Learning (P2PDRL).
+
+.. note::
+    Call with defining --max_steps.
+"""
+import torch as to
+import multiprocessing as mp
+
+import pyrado
+from pyrado.algorithms.step_based.p2pdrl import P2PDRL
+from pyrado.algorithms.step_based.ppo_gae import PPOGAE
+from pyrado.domain_randomization.default_randomizers import create_default_randomizer
+from pyrado.environment_wrappers.action_normalization import ActNormWrapper
+from pyrado.environment_wrappers.domain_randomization import DomainRandWrapperLive
+from pyrado.environments.pysim.quanser_qube import QQubeSwingUpSim
+from pyrado.logger.experiment import save_dicts_to_yaml, setup_experiment
+from pyrado.policies.recurrent.rnn import LSTMPolicy
+from pyrado.policies.feed_back.fnn import FNNPolicy
+from pyrado.policies.special.environment_specific import QQubeSwingUpAndBalanceCtrl
+from pyrado.spaces import ValueFunctionSpace
+from pyrado.utils.argparser import get_argparser
+from pyrado.utils.data_types import EnvSpec
+from multiprocessing import freeze_support
+
+parser = get_argparser()
+parser.add_argument("--freq", type=int, default=500)
+parser.add_argument("--num_workers", type=int, default=2)
+parser.add_argument("--max_iter", type=int, default=200)
+parser.add_argument("--num_epochs", type=int, default=250)
+
+if __name__ == "__main__":
+    # For multiprocessing and float32 support, recommended to include at top of script
+    freeze_support()
+    to.set_default_dtype(to.float32)
+
+    # Parse command line arguments
+    args = parser.parse_args()
+
+    # Set seed if desired
+    pyrado.set_seed(args.seed, verbose=True)
+    use_cuda = args.device == "cuda"
+    descr = f"_{args.max_steps}st_{args.freq}Hz_{args.num_teachers}t_{LSTMPolicy.name}"
+
+    # Environment
+    env_hparams = dict(dt=1 / args.freq, max_steps=args.max_steps)
+    env_real = QQubeSwingUpSim(**env_hparams)
+    ex_dir = setup_experiment(QQubeSwingUpSim.name, f"{PDDR.name}{descr}")
+
+    # Worker subroutine
+    worker_algo_hparam = dict(
+        max_iter=args.max_iter,
+        tb_name="ppo",
+        traj_len=args.max_steps,
+        gamma=0.999,
+        lam=0.997,
+        env_num=30,
+        cpu_num=12,  # int(mp.cpu_count()*2),
+        epoch_num=40,
+        device=args.device,
+        max_kl=0.05,
+        std_init=1.0,
+        clip_ratio=0.1,
+        lr=1e-3,
+        critic=critic,
+    )
+
+    worker_algo = PPOGAE
+
+    # Wrapper
+    randomizer = create_default_randomizer(env_real)
+    env_real = DomainRandWrapperLive(env_real, randomizer)
+    env_real = ActNormWrapper(env_real)
+
+    # Student policy
+    policy_hparam = dict(hidden_size=64, num_recurrent_layers=1, output_nonlin=to.tanh, use_cuda=use_cuda)
+    policy = LSTMPolicy(spec=env_real.spec, **policy_hparam)
+
+    # Subroutine
+    algo_hparam = dict(
+        max_iter=args.max_iter,
+        min_steps=args.max_steps,
+        num_cpu=20,  # int(mp.cpu_count()/2),
+        std_init=0.1,
+        num_epochs=args.num_epochs,
+        num_workers=args.num_workers,
+        device=args.device,
+        worker_algo=worker_algo,
+        worker_algo_hparam=worker_algo_hparam,
+    )
+
+    algo = P2PDRL(ex_dir, env_real, policy, **algo_hparam)
+
+    # Save the hyper-parameters
+    save_dicts_to_yaml(
+        dict(env=env_hparams, seed=args.seed),
+        dict(policy=policy_hparam),
+        dict(algo=algo_hparam, algo_name=algo.name),
+        save_dir=ex_dir,
+    )
+
+    # Jeeeha
+    algo.train(snapshot_mode="best", seed=args.seed)
